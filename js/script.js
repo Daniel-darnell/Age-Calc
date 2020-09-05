4c641a88bd7d4871e01cc736a0ee26a7baab9ee9
Daniel-darnell <darnelldanny1997@gmail.com>
2020-09-05 19:13:00 +0300

    Added detailed functions to the js function

--- js/script.js
+++ js/script.js
@@ -7,4 +7,20 @@ function getInput() {
     month = parseInt(document.getElementById("month").value);
     dayOfTheMonth = parseInt(document.getElementById("date").value);
 
+    if (century < 19 || century > 20) {
+        alert("Invalid Century Input")
+
+    }
+    else if (dayOfTheMonth < 1 || dayOfTheMonth > 32) {
+        alert("Invalid Date Input")
+
+    }
+    else if (monrh < 1 || month > 12) {
+        alert("Invalid Month Input")
+
+    }
+    else if (year < 1910 || year > 2020) {
+        alert{"Invalid Year Input"}
+    }
+
 }
