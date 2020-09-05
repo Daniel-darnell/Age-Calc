1669742ccbc2b6ceafe78f517073a66d52372038
Daniel-darnell <darnelldanny1997@gmail.com>
2020-09-05 19:04:45 +0300

    Added a js function

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Akan Names 
+
+
+function getInput() {
+    century = parseInt(document.getElementById)("century").value;
+    year = parseInt(document.getElementById("year").value);
+    month = parseInt(document.getElementById("month").value);
+    dayOfTheMonth = parseInt(document.getElementById("date").value);
+
+}
